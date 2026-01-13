0fc9e282df1e1a0d0fe8e79e30c52ce188e8d8cc
FTomasz <128514559+FTomasz@users.noreply.github.com>
2026-01-13 10:52:04 +0000

    Added login form
    Login form for user

--- js/views/login.js
+++ js/views/login.js
@@ -3,6 +3,21 @@ export function renderLogin(container) {
     <section class="row justify-content-center">
       <div class="col-md-6 col-lg-4">
         <h2 class="mb-4 text-center">Login</h2>
+        <form id="login-form">
+          <div class="mb-3">
+            <label class="form-label">Username</label>
+            <input type="text" class="form-control" required>
+          </div>
+
+          <div class="mb-3">
+            <label class="form-label">Password</label>
+            <input type="password" class="form-control" required>
+          </div>
+
+          <button class="btn">
+            Login
+          </button>
+        </form>
       </div>
     </section>
   `;

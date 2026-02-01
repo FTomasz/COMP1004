db53f3e20c398ffc04d95c4a304c50cb26859eda
FTomasz <128514559+FTomasz@users.noreply.github.com>
2026-02-01 14:05:17 +0000

    Added buttons to the dashboard

--- js/views/dashboard.js
+++ js/views/dashboard.js
@@ -1,9 +1,37 @@
 export function renderDashboard(container) {
   container.innerHTML = `
-  <section class="row justify-content-center">
-      <div class="col-md-6 col-lg-4">
-        <h2 class="mb-4 text-center">Dashboard</h2>
-        
+    <section class="container py-4">
+      <div class="row justify-content-center mb-4">
+        <div class="col-12 col-lg-8">
+          <h2 class="text-center mb-0">Dashboard</h2>
+        </div>
+      </div>
+
+      <div class="row g-3 justify-content-center">
+
+        <div class="col-12 col-md-6 col-lg-3">
+          <button id="dashboard-btn-create" class="btn btn-create btn-lg w-100 py-4">
+            Create Itinerary
+          </button>
+        </div>
+
+        <div class="col-12 col-md-6 col-lg-3">
+          <button id="dashboard-btn-edit" class="btn btn-edit btn-lg w-100 py-4">
+            Edit Itinerary
+          </button>
+        </div>
+
+        <div class="col-12 col-md-6 col-lg-3">
+          <button id="dashboard-btn-publish" class="btn btn-publish btn-lg w-100 py-4">
+            Publish Itinerary
+          </button>
+        </div>
+
+        <div class="col-12 col-md-6 col-lg-3">
+          <button id="dashboard-btn-delete" class="btn btn-delete btn-lg w-100 py-4">
+            Delete Itinerary
+          </button>
+        </div>
       </div>
     </section>
   `;

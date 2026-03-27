2a3e6cca5abb9210bc407df081e7cd6ac223297c
FTomasz <128514559+FTomasz@users.noreply.github.com>
2026-03-27 13:53:35 +0000

    plan for publish

--- js/views/publishItinerary.js
+++ js/views/publishItinerary.js
@@ -1,10 +1,12 @@
 export function renderPublishItinerary(container) {
+  const id = sessionStorage.getItem("active-itinerary-id");
+
   container.innerHTML = /*html*/ `
     <section class="container py-4">
       <div class="row justify-content-center mb-4">
         <div class="col-12 col-lg-10">
           <div class="d-flex flex-column flex-md-row justify-content-between align-items-md-center gap-3">
-            <h2 class="mb-0">Dashboard</h2>
+            <h2 class="mb-0">Publish Itinerary</h2>
 
             <div class="d-flex gap-2">
               <button id="facebook-publish-btn" class="btn publish-button">
@@ -25,7 +27,7 @@ export function renderPublishItinerary(container) {
         <div class="col-12 col-lg-10">
           <div class="card">
             <div class="card-body">
-              <h5 class="mb-3">Preview itinerary</h5>
+              <h5 class="mb-3">Preview</h5>
               <div id="itinerary-preview" class="d-flex flex-column gap-2"></div>
             </div>
           </div>
@@ -33,8 +35,21 @@ export function renderPublishItinerary(container) {
       </div>
     </section>
   `;
+
+  const preview = document.getElementById("itinerary-preview");
+
+  if (!itinerary){
+    preview.innerHTML =  `<div class="card"><div class="card-body text-muted"> Error: No itinerary found.</div></div>`
+    return;
+  }
 }
 
+
+//add overview of the itinerary, with basic details listed
+
+//reuse code from edit itinerary removing all the editing buttons etc.
+
+//show days and events, add all text to one card so its easier to export afterwards
 
 
 function escapeHtml(str) {
